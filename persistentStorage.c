320ae5a7014087914b6613796355932bd906fac4
lukas <lubra101@uni-duesseldorf.de>
2023-06-04 19:17:34 +0200

    writeToStorage weitergeschrieben

--- persistentStorage.c
+++ persistentStorage.c
@@ -9,36 +9,39 @@
 int writeToStorage(wQueue *const q, char *const term, const size_t len, const char digest[HASH_LEN]) {
     char readableHash[READABLE_HASH_LEN];
     print_readable_digest(digest, readableHash);
+    int fd;
 
-    char dir[48] = "";
-    char lastPath[48] = "";
-    char firstElementPath[48] = "";
-    char folder[] = "storage/";
-    strcpy(dir, folder);
+    char dir[41] = "storage/"; // 41 = 8 + 32 + 1
     strcat(dir, readableHash);
 
-    int fd = open(dir, O_CREAT, 00700);
-
-    if (fd == -1) {
-        perror("Error in opening file");
-        return -1;
+    if ((fd = open(dir, O_RDWR | O_CREAT | O_TRUNC, 00600)) == -1) {
+        perror("Cannot open output file\n");
+        exit(1);
     }
-    char *buf = (char *)malloc(sizeof(char) * (20 + 8 + len));
-    // printf("1 digest:%s\n", digest);
-    // printf("2 len:%ld\n", len);
-    // printf("3 term:%s\n\n", term);
 
-    char charlen[8];
-    sprintf(charlen, "%ld", len);
+    // printf("digest: %s\n",digest);
+    // printf("len: %ld\n",len);
+    // printf("term: %s\n",term);
 
-    strcpy(buf, digest);
-    strcat(buf, charlen);
-    strcat(buf, term);
-    // printf("buf: %s",buf);
+    char buf[28 + len];
 
-    write(fd, &buf, strlen(buf));
-    free(buf);
+    memcpy(buf, digest, 20);
+    memcpy(buf + 20, &len, sizeof(size_t));
+    memcpy(buf + 28, term, len);
 
-    close(fd);
+    write(fd, buf, (28 + len));
+
+    /*
+    size_t len2 = 0;
+    char digest2[HASH_LEN] = {0};
+
+    read(fd, buf, 28);
+
+    memcpy(&len2, buf + 20, sizeof(size_t));
+    memcpy(digest2, buf, 20);
+
+    printf("len2: %ld\n",len2);
+    printf("digest2: %s\n",digest2);*/
+
     return 0;
 }
