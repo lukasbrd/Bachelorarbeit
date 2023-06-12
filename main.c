537f5e6814d2be51c5cbdacd08ee2c247ab7ef4d
lukas <lubra101@uni-duesseldorf.de>
2023-06-12 22:54:44 +0200

    readAllFromStorage() weitergearbeitet

--- main.c
+++ main.c
@@ -6,24 +6,34 @@
 
 int main(void) {
     wQueue *q = init_queue();
-    size_t len = 100;
+    size_t len = 0;
     tCell *res = NULL;
     char digest[HASH_LEN] = "";
     char *term;
     srand(time(NULL));
 
+    readAllFromStorageToQueue(q); 
+
+    /*
     // Thread1:
     int i;
-    for (i = 0; i < 1; i++) {
+    for (i = 0; i < 3; i++) {
         term = createRandomString(term);
         len = strlen(term);
         hash(term, len, digest);
         enqueue(q, term, len, digest);
-        writeToStorage(q,term,len,digest);
+        writeToStorage(q, term, len, digest);
     }
 
+    // Thread2:
+    res = dequeue(q);
+    deleteFromStorage(res->digest);
+    free(res->term);
+    free(res);
+    */
+
     printf("\n\n----------------------------------------------------------------\n");
-    printf("Length of Queue: %lu\n", q_size(q));
+    printf("Length of Queue: %lu\n\n", q_size(q));
     printAllTermsOfCells(q);
 
     teardown_queue(q);
--- persistentStorage.c
+++ persistentStorage.c
@@ -1,6 +1,7 @@
 #include "hash.h"
 #include "queue.h"
 #include <assert.h>
+#include <dirent.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
@@ -11,17 +12,13 @@ int writeToStorage(wQueue *const q, char *const term, const size_t len, const ch
     print_readable_digest(digest, readableHash);
     int fd;
 
-    char dir[41] = "storage/"; // 41 = 8 + 32 + 1
+    char dir[41] = "storage/";
     strcat(dir, readableHash);
 
     if ((fd = open(dir, O_RDWR | O_CREAT | O_TRUNC, 00600)) == -1) {
         perror("Cannot open output file\n");
         exit(1);
     }
-
-    // printf("digest: %s\n",digest);
-    // printf("len: %ld\n",len);
-    // printf("term: %s\n",term);
 
     char buf[28 + len];
 
@@ -30,18 +27,46 @@ int writeToStorage(wQueue *const q, char *const term, const size_t len, const ch
     memcpy(buf + 28, term, len);
 
     write(fd, buf, (28 + len));
+    close(fd);
+    return 0;
+}
 
-    /*
-    size_t len2 = 0;
-    char digest2[HASH_LEN] = {0};
+int deleteFromStorage(const char digest[HASH_LEN]) {
+    char readableHash[READABLE_HASH_LEN];
+    print_readable_digest(digest, readableHash);
+    int fd;
 
-    read(fd, buf, 28);
+    char dir[41] = "storage/";
+    strcat(dir, readableHash);
 
-    memcpy(&len2, buf + 20, sizeof(size_t));
-    memcpy(digest2, buf, 20);
+    int del = remove(dir);
+}
 
-    printf("len2: %ld\n",len2);
-    printf("digest2: %s\n",digest2);*/
+int readAllFromStorageToQueue(wQueue *const q) {
+    size_t len;
+    char digest[HASH_LEN];
+    char buf[28];
+    int fd;
 
+    DIR *dir;
+    struct dirent *dp;
+
+    dir = opendir("storage/");
+    if (dir != NULL) {
+        while (dp = readdir(dir)) {
+            if (strcmp(dp->d_name, ".") && strcmp(dp->d_name, "..")) {
+                fd = openat(dirfd(dir), dp->d_name, O_RDONLY);
+                read(fd, buf, 28);
+                memcpy(digest, buf, 20);
+                memcpy(&len, buf + 20, sizeof(size_t));
+                char term[len];
+                read(fd + 28,term,len);
+                close(fd);
+            }
+        }
+    } else {
+        perror("Couldn't open the directory");
+    }
+    closedir(dir);
     return 0;
 }
