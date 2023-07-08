2f9ed0f596887bdba863749b5301abd4c72cdf78
lukas <lubra101@uni-duesseldorf.de>
2023-07-08 13:52:23 +0200

    header

---
+++ queue.h
@@ -0,0 +1,54 @@
+#include "hash.h"
+#include <pthread.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// Definition einer Zelle
+// size_t ist ein unsigned int
+typedef struct Cell {
+    char *term;
+    char digest[HASH_LEN];
+    size_t term_length;
+    struct Cell *next;
+} tCell;
+
+// Definition der Queue mit:
+// Pointer auf die erste Zelle
+// Pointer auf die Zweite Zelle
+// Groesse der Queue als unsigned int
+typedef struct workQueue {
+    tCell *first;
+    tCell *last;
+    size_t c;
+    tCell *first_not_in_mem;
+} wQueue;
+
+typedef struct {
+    wQueue *q;
+    char *storageTerm;
+    size_t len;
+    char digest[HASH_LEN];
+} StorageData;
+
+typedef struct {
+    wQueue *q;
+    char *queueTerm;
+    size_t len;
+    char digest[HASH_LEN];
+} QueueData;
+
+wQueue *init_queue();
+void enqueue(wQueue *const q, char *const term, const size_t len, char digest[HASH_LEN]);
+tCell *dequeue(wQueue *const q);
+void teardown_queue(wQueue *q);
+int is_empty(wQueue const *const q);
+size_t q_size(wQueue const *const q);
+void printAllTermsOfCells(wQueue const *const q);
+
+int writeToStorage(wQueue *const q, char *const term, const size_t len, char digest[HASH_LEN]);
+int deleteFromStorage(const char digest[HASH_LEN]);
+int readAllFromStorageToQueue(wQueue *const q);
+int readOneTermFromStorageToQueue(wQueue *const q);
+void enqueueWithoutTerm(wQueue *const q, char *const term, const size_t len, const char digest[HASH_LEN]);
+void enqueueTerm(wQueue *const q, char *const term, const size_t len, const char digest[HASH_LEN]);
+tCell *dequeueTerm(wQueue *const q);
