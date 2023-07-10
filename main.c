b2a0b6fcf7688d3929c96840dd643064a3eb6900
lukas <lubra101@uni-duesseldorf.de>
2023-07-11 00:54:15 +0200

    parallel works inside the loop

--- main.c
+++ main.c
@@ -20,12 +20,14 @@ void *enqueueTerm(void *input) {
     writeToStorage(term, len, digest);
     enqueue(q, term, len, digest);
     atomic_fetch_add(&q->c, 1);
+    pthread_exit(NULL);
 }
 
 void *dequeueTerm(void *input) {
     struct Data *data = (struct Data *)input;
     tCell *res = NULL;
     wQueue *q = data->q;
+    while (1) {
         if (atomic_load(&q->c) > 0) {
             if (atomic_load(&q->c) - atomic_load(&q->in_mem) > 0) {
                 readOneTermFromStorageToQueue(q);
@@ -37,34 +39,38 @@ void *dequeueTerm(void *input) {
             free(res);
             return res;
         }
+    }
 }
 
-    int main(void) {
-        wQueue *q = init_queue();
-        srand(time(NULL));
+int main(void) {
+    wQueue *q = init_queue();
+    srand(time(NULL));
 
-        pthread_t enqueue;
-        pthread_t dequeue;
+    pthread_t enqueue;
+    pthread_t dequeue;
 
-        for (int i = 0; i < 4; i++) {
-            char *term = createRandomString(term);
-            struct Data data;
-            printf("First:%s\n", term);
-            data.q = q;
-            data.term = term;
-            printf("Second:%s\n", term);
+    for (int i = 0; i < 4; i++) {
+        char *term = createRandomString(term);
+        struct Data data;
+        printf("First:%s\n", term);
+        data.q = q;
+        data.term = term;
+        printf("Second:%s\n", term);
 
-            pthread_create(&enqueue, NULL, enqueueTerm, (void *)&data);
-            pthread_join(enqueue, NULL);
-            pthread_create(&dequeue, NULL, dequeueTerm, (void *)&data);
-            pthread_join(dequeue, NULL);
-        }
-
-        printf("\n\n----------------------------------------------------------------\n");
-        printf("Length of Queue: %lu\n\n", q_size(q));
-        printAllTermsOfCells(q);
-        printf("ENDE\n");
-
-        teardown_queue(q); // freed den Rest der Queue und die Q selbst!!!
-        return 0;
+        pthread_create(&enqueue, NULL, enqueueTerm, (void *)&data);
+        pthread_create(&dequeue, NULL, dequeueTerm, (void *)&data);
+        
+        pthread_join(enqueue, NULL);
+        pthread_join(dequeue, NULL);
     }
+
+
+
+    printf("\n\n----------------------------------------------------------------\n");
+    printf("Length of Queue: %lu\n\n", q_size(q));
+    printAllTermsOfCells(q);
+    printf("ENDE\n");
+
+    teardown_queue(q); // freed den Rest der Queue und die Q selbst!!!
+    return 0;
+}
