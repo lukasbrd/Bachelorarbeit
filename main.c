9437a951b82a8e4599afbe0ec6def94c332a4670
lukas <lubra101@uni-duesseldorf.de>
2023-07-14 16:06:29 +0200

    zeroMQ Test

--- main.c
+++ main.c
@@ -1,62 +1,43 @@
-#include "queue.h"
-#include "randomService.h"
-#include "stdio.h"
-#include "string.h"
+#include <assert.h>
 #include <pthread.h>
-#include <time.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <zmq.h>
 
-void enqueueTerm(wQueue *const q, char *const term, const size_t len, char digest[HASH_LEN]) {
-    q->c++;
-    writeToStorage(term, len, digest);
-    if (q->c < 3) {
-        enqueue(q, term, len, digest);
-    } else {
-        enqueueWithoutTerm(q, term, len, digest);
-    }
-}
+void *send_hello(void *socket) {
+    void *requester = zmq_socket(socket, ZMQ_REQ);
+    zmq_connect(requester, "tcp://localhost:5555");
 
-tCell *dequeueTerm(wQueue *const q) {
-    if (q->c == 0) {
-        return NULL;
+    while (1) {
+        sleep(1); // Delay between messages
+        zmq_send(requester, "Hello", 5, 0);
+
+        char buffer[10];
+        zmq_recv(requester, buffer, 10, 0);
+        printf("Received: %s\n", buffer);
     }
-    if (q->c >= 3) {
-        readOneTermFromStorageToQueue(q);
-        if (q->first_not_in_mem->next != NULL) {
-            q->first_not_in_mem = q->first_not_in_mem->next;
-        }
-    }
-    q->c--;
-    return dequeue(q);
+
+    zmq_close(requester);
+    return NULL;
 }
 
 int main(void) {
-    wQueue *q = init_queue();
-    // readAllFromStorageToQueue(q);
-    srand(time(NULL));
-    char digest[HASH_LEN] = "";
-    char *term;
-    size_t len = 0;
-    tCell *res = NULL;
+    //  Socket to talk to clients
+    void *context = zmq_ctx_new();
+    void *responder = zmq_socket(context, ZMQ_REP);
+    int rc = zmq_bind(responder, "tcp://*:5555");
+    assert(rc == 0);
 
-    int i;
-    for (i = 0; i < 4; i++) {
-        term = createRandomString(term);
-        len = strlen(term);
-        hash(term, len, digest);
-        enqueueTerm(q, term, len, digest);
+    pthread_t send_thread;
+    pthread_create(&send_thread, NULL, send_hello, context);
+
+    while (1) {
+        char buffer[10];
+        zmq_recv(responder, buffer, 10, 0);
+        printf("Received Hello\n");
+        sleep(1); //  Do some 'work'
+        zmq_send(responder, "World", 5, 0);
     }
-
-    while (q->c > 0) {
-        res = dequeueTerm(q);
-        free(res->term);
-        free(res);
-    }
-
-    printf("\n\n----------------------------------------------------------------\n");
-    printf("Length of Queue: %lu\n\n", q_size(q));
-    printAllTermsOfCells(q);
-    printf("ENDE\n");
-
-    teardown_queue(q);
     return 0;
 }
