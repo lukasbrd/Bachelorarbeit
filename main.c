5d612df628d6f406994a14a1e916896064de2edb
lukas <lubra101@uni-duesseldorf.de>
2023-07-16 21:18:38 +0200

    running but with error

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include "queue.h"
+#include "randomService.h"
 #include "stdio.h"
 #include <czmq.h>
-#include "randomService.h"
 
 #define ENQUEUE 1
 #define DEQUEUE 2
+
+bool runner = true;
 
 typedef struct Cell {
     char *term;
@@ -12,24 +14,53 @@ typedef struct Cell {
     char digest[HASH_LEN];
 } tCell;
 
-void enqueue(zsock_t *pushSocket ,char *term) {
+void *threaddi(void *args) {
+
+    zsock_t *commandSocket = zsock_new_pull("inproc://command");
+    zsock_t *packageSocket = zsock_new_push("inproc://package");
+
+    zsock_t *enqueue = zsock_new_push("inproc://queue");
+    zsock_t *dequeue = zsock_new_pull("inproc://queue");
+
+    while (runner) {
+        int cmd;
+        tCell *cell;
+        int rc = zsock_recv(commandSocket, "ip", &cmd, &cell);
+        assert(rc == 0);
+
+        if (cmd == ENQUEUE) {
+            zsock_send(enqueue, "p", cell);
+        } else if (cmd == DEQUEUE) {
+            tCell *receivedCell;
+            zsock_recv(dequeue, "p", &receivedCell);
+            zsock_send(packageSocket, "p", receivedCell);
+        }
+    }
+    zsock_destroy(&commandSocket);
+    zsock_destroy(&packageSocket);
+    zsock_destroy(&enqueue);
+    zsock_destroy(&dequeue);
+
+    return NULL;
+}
+
+void enqueue(zsock_t *commandSocket, char *term) {
     tCell *cell = malloc(sizeof(tCell));
     cell->term = NULL;
     cell->term_length = strlen(term);
 
     hash(term, cell->term_length, cell->digest);
 
-    // Push the struct to the queue
-    zsock_send(pushSocket,"ip",1, cell);
-    writeToStorage(term,cell->term_length,cell->digest);
+    zsock_send(commandSocket, "ip", ENQUEUE, cell);
+    writeToStorage(term, cell->term_length, cell->digest);
     free(term);
 }
 
-tCell *dequeue(zsock_t *pullSocket) {
-    // Pull the struct from the queue
+tCell *dequeue(zsock_t *command, zsock_t *packageSocket) {
     tCell *receivedCell;
-    int cmd;
-    zsock_recv(pullSocket,"ip",&cmd,&receivedCell);
+    zsock_send(command, "ip", DEQUEUE, NULL);
+    zsock_recv(packageSocket, "p", &receivedCell);
+
     receivedCell->term = readOneTermFromStorage(receivedCell->digest);
     return receivedCell;
 }
@@ -38,30 +69,35 @@ int main(void) {
     pthread_t thread;
     srand(time(NULL));
 
-    //bind one socket/longer living one
-    zsock_t *pushSocket = zsock_new_push("inproc://queue");
+    pthread_create(&thread, NULL, threaddi, NULL);
+    sleep(3);
 
-    //connect the other socket
-    zsock_t *pullSocket = zsock_new_pull("inproc://queue");
+    zsock_t *commandSocket = zsock_new_push("inproc://command");
+    zsock_t *packageSocket = zsock_new_pull("inproc://package");
 
-    char *term = createRandomString();
-    printf("TermStart:%s\n",term);
+    for (int i = 0; i < 4; i++) {
+        char *term = createRandomString();
+        printf("TermStart:%s\n", term);
 
-    enqueue(pushSocket,term);
+        enqueue(commandSocket, term);
 
-    tCell *receivedCell;
-    receivedCell = dequeue(pullSocket);
+        tCell *receivedCell;
+        receivedCell = dequeue(commandSocket, packageSocket);
 
-    // Print the values of the received struct
-    printf("Received struct:\n");
-    printf("receivedTerm Length: %ld\n", receivedCell->term_length);
-    printf("receivedTerm : %s\n", receivedCell->term);
+        printf("Received struct:\n");
+        printf("receivedTerm Length: %ld\n", receivedCell->term_length);
+        printf("receivedTerm : %s\n", receivedCell->term);
 
-    // Cleanup
-    free(receivedCell->term);
-    free(receivedCell);
-    zsock_destroy(&pushSocket);
-    zsock_destroy(&pullSocket);
+        free(receivedCell->term);
+        free(receivedCell);
+    }
+
+    runner = false;
+
+    pthread_cancel(thread);
+    pthread_join(thread, NULL);
+    zsock_destroy(&commandSocket);
+    zsock_destroy(&packageSocket);
 
     return 0;
 }
