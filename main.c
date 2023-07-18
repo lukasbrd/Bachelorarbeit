ff484e9ee40fcade5cfca3b117cbac59c53c7044
lukas <lubra101@uni-duesseldorf.de>
2023-07-18 09:30:29 +0200

    only 2 of 4 Terms are enqueued

--- main.c
+++ main.c
@@ -20,6 +20,8 @@ void *threaddi(void *args) {
     zsock_t *enqueue = zsock_new_push("inproc://queue");
     zsock_t *dequeue = zsock_new_pull("inproc://queue");
 
+    int queueLength = 0;
+
     while (1) {
         int cmd;
         tCell *cell;
@@ -27,6 +29,10 @@ void *threaddi(void *args) {
         assert(rc == 0);
 
         if (cmd == ENQUEUE) {
+            if (queueLength >= 3) {
+                free(cell->term);
+                cell->term = NULL;
+            }
             zsock_send(enqueue, "p", cell);
         } else if (cmd == DEQUEUE) {
             tCell *receivedCell;
@@ -44,14 +50,11 @@ void *threaddi(void *args) {
 
 void enqueue(zsock_t *commandSocket, char *term) {
     tCell *cell = malloc(sizeof(tCell));
-    cell->term = NULL;
+    cell->term = term;
     cell->term_length = strlen(term);
-
     hash(term, cell->term_length, cell->digest);
-
     zsock_send(commandSocket, "ip", ENQUEUE, cell);
-    writeToStorage(term, cell->term_length, cell->digest);
-    free(term);
+    writeToStorage(cell->term, cell->term_length, cell->digest);
 }
 
 tCell *dequeue(zsock_t *command, zsock_t *packageSocket) {
@@ -59,7 +62,9 @@ tCell *dequeue(zsock_t *command, zsock_t *packageSocket) {
     zsock_send(command, "ip", DEQUEUE, NULL);
     zsock_recv(packageSocket, "p", &receivedCell);
 
-    receivedCell->term = readOneTermFromStorage(receivedCell->digest);
+    if (receivedCell->term == NULL) {
+        receivedCell->term = readOneTermFromStorage(receivedCell->digest);
+    }
     return receivedCell;
 }
 
